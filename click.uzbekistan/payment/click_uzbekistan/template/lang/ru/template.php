d174dafb2c60093c128cac759ccd657f0989a3da
Pardayev Akbar <a.pardayev@corp.click.uz>
2020-01-30 20:10:55 +0500

    Encoding issue on Pay button

--- click.uzbekistan/payment/click_uzbekistan/template/lang/ru/template.php
+++ click.uzbekistan/payment/click_uzbekistan/template/lang/ru/template.php
@@ -7,5 +7,5 @@ $MESS['SALE_HANDLERS_CLICK_UZ_WARNING_RETURN'] = <<<HTML
 <b>Обратите внимание:</b> если вы откажетесь от покупки, для возврата денег вам придется обратиться в магазин.
 HTML;
 $MESS['SALE_HANDLERS_CLICK_UZ_REDIRECT']       = 'вы будете перенаправлены на страницу оплаты';
-$MESS['SALE_HANDLERS_CLICK_UZ_BUTTON_PAID']    = 'Оплатить';
+$MESS['SALE_HANDLERS_CLICK_UZ_BUTTON_PAID']    = 'Оплатить через CLICK';
 $MESS['SALE_HANDLERS_CLICK_UZ_REDIRECT_MESS']  = 'Вы будете перенаправлены на страницу оплаты';
